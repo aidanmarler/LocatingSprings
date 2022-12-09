6ca8c48ca0f41dfbaa3295536a9c09f3c7c85f69
aidanmarler <aidanmarler1@gmail.com>
2022-12-09 13:23:06 -0700

    Updated Help Prompt

--- js/helpPrompt.js
+++ js/helpPrompt.js
@@ -43,26 +43,35 @@ helpIcon.addEventListener("mouseout", function () {
 
 // Add text to fill the help prompt here!
 // Title
-helpWindow.innerHTML = "<font size='+3'><b>Exploring the springs of Northern Nebraska</b></font>"
+helpWindow.innerHTML = "<font size='+3'><b>Exploring the Springs of Northern Nebraska</b></font>"
 
-helpWindow.innerHTML += "<br><br>Jay Kong and Aidan Marler, Final Project for Dr. Karimzadeh's Geog 4043"
+helpWindow.innerHTML += "<br><br><i>Made by Jay Kong and Aidan Marler, Final Project for Dr. Karimzadeh's Geog 4043</i>"
+helpWindow.innerHTML += "<br>We recommend that you use this app in <b>Google Chrome</b> and on a computer, as opposed to a mobile device"
 
 // PCP Instructions
 helpWindow.innerHTML += "<br><br><font size='+2'><b>Parallel Coordinate Plot</b></font"
-helpWindow.innerHTML += "<br>The large lined graph is a <b>Parallel Coordinate Plot.</b>  Colors on this graph represent Hydrolic Groups."
-helpWindow.innerHTML += "It can be interacted with in a few ways"
-helpWindow.innerHTML += "<br> - To filter, click and drag on any of the verical axes.  To remove a filter, click anywhere else on that axis."
-helpWindow.innerHTML += "<br> - To reorder the axes, click and drag their label."
-helpWindow.innerHTML += "<br> - Hovering over any spring point on the map will display just that line.  "
+helpWindow.innerHTML += "<br>The large lined graph is a <b>Parallel Coordinate Plot. </b>"
+helpWindow.innerHTML += "<br> <b>-</b> Colors on this graph represent <b>Hydrologic Groups.</b>"
+helpWindow.innerHTML += "<br> <b>-</b> To filter, click and drag on any of the verical axes.  To remove a filter, click anywhere else on that axis."
+helpWindow.innerHTML += "<br> <b>-</b> To reorder the axes, click and drag their label."
+helpWindow.innerHTML += "<br> <b>-</b> Hovering over any spring point on the map will display just that line on the parallel coordinate plot.  "
 
 // Map Instructions
 helpWindow.innerHTML += "<br><br><font size='+2'><b>Map</b></font"
-helpWindow.innerHTML += "<br> "
-helpWindow.innerHTML += "It can be interacted with in a few ways"
-helpWindow.innerHTML += "<br> - By clicking on the map, a marker will be placed.  Information about this point can be querried by hoving over the marker."
-helpWindow.innerHTML += "<br> - The checkboxes allow you to set a layer on the map to be active or not.  They are transparent, allowing the visualization of multiple layers at once."
+helpWindow.innerHTML += "<br> <b>-</b> A <b>marker</b> can be placed by clicking on the map.  Hovering over the marker will reveal information about that point."
+helpWindow.innerHTML += "<br> - - The marker can be moved by clicking somewhere else on the map and can be removed by clicking on the existing marker"
+helpWindow.innerHTML += "<br> - - - If the marker is not showing its data or is not disapearing after clicking it, try clicking around the map to reset the marker.  If the marker is still not working, reloading the page will fix the issue."
+helpWindow.innerHTML += "<br> <b>-</b> The <b>checkboxes</b> allow you to set a layer on the map to be active or not.  They are transparent, allowing the visualization of multiple layers at once."
+helpWindow.innerHTML += "<br> <b>-</b> The <b>legend</b> reflects the top-most active layer.  The farther right a layer is in the checkbox window, the lower it is in relation to the other layers."
+helpWindow.innerHTML += "<br> <b>-</b> The <b>base layer</b> can be changed with the layers icon on the top right of the map window."
+
 // Matrix Instructions
+helpWindow.innerHTML += "<br><br><font size='+2'><b>Heatmap</b></font"
+helpWindow.innerHTML += "<br>The <b>Heatmap</b> is the black and blue graph on the right side of the screen."
+helpWindow.innerHTML += "<br> <b>-</b> The intensity of blue of each box reflects the number of springs in that category."
+helpWindow.innerHTML += "<br> <b>-</b> By <b>hovering</b> over a box, the number of springs will be displayed at the bottom of the graph."
 
 // Further help, link the video
-
+helpWindow.innerHTML += "<br><br><br> <i>For more assitance with the application, see <b><a href='https://youtu.be/Aonnehx7d1k' target='_blank'>this video.</a></b> Or, you can email us at <a href='mailto:aima7917@colorado.edu' target='_blank'>aima7917@colorado.edu</a>.</i>"
+helpWindow.innerHTML += "<br> We sourced our <a href='https://mrdata.usgs.gov/geology/state/state.php?state=NE' target='_blank'>data on geology from here</a>, our <a href='https://apps.nationalmap.gov/downloader/' target='_blank'>data on topography and hydrology here</a>, and our <a href='https://data.nal.usda.gov/dataset/gridded-soil-survey-geographic-database-gssurgo' target='_blank'>and our data on soils from here</a>."
 // Data Used and further resources, link the data used (geology, topography, soils)
